f163fa2175d32bc1da3bfd4eaf713e69de5935d1
Siddhartha Sharma <siddhartha1234sharma@gmail.com>
2025-09-04 20:34:35 +0530

    chore: remove MDX support and cleanup

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
-import createMDX from "@next/mdx"
-
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-});
 
 const nextConfig: NextConfig = {
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 };
 
-export default withMDX(nextConfig);
+export default nextConfig;
